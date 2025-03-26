eac16b24dc9224ef0bfa41e3f5db31334a806bb5
HelinduS <136826228+HelinduS@users.noreply.github.com>
2025-03-26 11:34:06 +0530

    Delete backend directory

--- backend/src/main/java/com/amri/login2/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.amri.login2.repository;
-
-import com.amri.login2.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-    Users findByEmail(String email);
-}
