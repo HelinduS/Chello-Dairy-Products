13f45f2b7eea92276a67abfb513c65e2825aae1f
HelinduS <helindusenadheera@gmail.com>
2025-04-02 01:02:45 +0530

    handled some errors

--- chello-backend/src/main/java/com/chello/milkdelivery/repository/OrderRepository.java
+++ chello-backend/src/main/java/com/chello/milkdelivery/repository/OrderRepository.java
@@ -1,6 +1,9 @@
 package com.chello.milkdelivery.repository;
 
 import com.chello.milkdelivery.model.Order;
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
