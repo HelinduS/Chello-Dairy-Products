75530ea5b69b5f41fc0dff14cb4f0d3a9bed912f
it23392830 <it23392830@my.sliit.lk>
2025-04-02 09:37:44 +0530

    Product.java file

---
+++ chello-backend/src/main/java/com/chello/milkdelivery/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.chello.milkdelivery.repository;
+
+import com.chello.milkdelivery.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByProductId(int productId);
+
+}
