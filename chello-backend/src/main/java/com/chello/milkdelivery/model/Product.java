f8411ad0104df5b7897d363f2f8581716ba98c4d
it23392830 <it23392830@my.sliit.lk>
2025-04-02 09:35:50 +0530

    Product.java file

---
+++ chello-backend/src/main/java/com/chello/milkdelivery/model/Product.java
@@ -0,0 +1,27 @@
+package com.chello.milkdelivery.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "product")
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "productid")
+    private int productId;
+
+    @Column(name = "product")
+    @NotBlank(message = "Product Name Required")
+    private String product;
+
+    @Column(name = "unitprice")
+    private double unitPrice;
+
+}
