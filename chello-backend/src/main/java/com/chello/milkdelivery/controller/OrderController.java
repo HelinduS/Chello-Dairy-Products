6b08f94f5a3a2ab5f2a461efffb1d0694cd4217f
amri0faz31 <140976375+amri0faz31@users.noreply.github.com>
2025-04-22 15:39:53 +0530

    ordercontroller and orederRpo

--- chello-backend/src/main/java/com/chello/milkdelivery/controller/OrderController.java
+++ chello-backend/src/main/java/com/chello/milkdelivery/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.chello.milkdelivery.dto.OrderStatusUpdateDto;
 import com.chello.milkdelivery.dto.PlaceOrderDto;
 import com.chello.milkdelivery.model.Order;
 import com.chello.milkdelivery.repository.OrderRepository;
+import com.chello.milkdelivery.service.OrderStatisticsService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
--- chello-backend/src/main/java/com/chello/milkdelivery/repository/OrderRepository.java
+++ chello-backend/src/main/java/com/chello/milkdelivery/repository/OrderRepository.java
@@ -3,6 +3,9 @@ package com.chello.milkdelivery.repository;
 import com.chello.milkdelivery.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByOrderDateBetween(LocalDate startDate, LocalDate endDate);
 }
